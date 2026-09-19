9fa9e9ab0ca0f4fcec463edd257d58328d6fb4e9
clementeivetta <clementeivetta@gmail.com>
2026-09-19 18:12:30 -0300

    Actualizar URL para el nuevo dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://c-ai.github.io', // Reemplaza TU_USUARIO
-  base: '/c-ai',               // El nombre de tu repositorio
+  site: 'https://c-ai.github.io',
   integrations: [react()],
 });
